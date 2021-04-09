27eb9f0d5d9a12f3cb6e6eaaa0f66fa842f55a56
Joel Straley <joelstraley@gmail.com>
2021-04-08 22:04:07 -0400

    folder structure added

---
+++ client/src/components/Psuedcode.js
@@ -0,0 +1,23 @@
+
+/* 
+
+Components (with Links and Props): 
+Navbar (links: login, Create Account)
+Search
+Carousel
+Events Container 
+Card SWIPE Block 
+Card flyer (link to EVENT pages)
+BigAButton
+About Us 
+MeetTeam
+Footer 
+
+
+Pages: 
+MAIN 
+Event Page 
+Register/Login
+All Events Page (Future / Main / Most Popular)
+
+ */
