c7e8ccffe1a4e411c6a776dac8d267ea7c7bd158
Joel Straley <joelstraley@gmail.com>
2021-04-13 22:03:07 -0400

    user schema built out

---
+++ client/controllers/showsController.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+      db.Show
+        .find(req.query)
+        .sort({ date: -1 })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+      db.Show
+        .findById(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+      db.Show
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+      db.Show
+        .findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+      db.Show
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
+  };
+  
+
--- client/models/index.js
+++ client/models/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     Show: require("./show")
+    User: require("./user")
   };
   
