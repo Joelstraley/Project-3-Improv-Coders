a3f47d73ad4e361f11fd72849aaa02b4ed9493f1
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-15 15:43:20 -0400

    created login components files

---
+++ client/src/pages/RegisterLogin/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Login from ("../components/Login/index.js")
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <Login />
+            </div>
+        )
+    }
+}
