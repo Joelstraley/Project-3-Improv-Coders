dd9a82873cba9f44f0be53281e2595829eb9b4e5
kofithebass <kofi@kofihayford.com>
2021-04-15 21:00:59 -0400

    added basic react skeleton to navbar and search files

---
+++ client/src/components/Search/search.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Search() {
+    return (
+        <div>
+            <button> Search Now </button>
+            <input id="search" placeholder="Search"> </input>
+
+        </div>)
+}
