f998520fb963f55fc52944e2472f6f47bc53b251
kofithebass <kofi@kofihayford.com>
2021-04-15 20:37:15 -0400

    renamed the files for better organizaiton

---
+++ client/src/components/Search/search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+    return (
+        <div>
+            <button> Search Now </button>
+            <input id="search" placeholder="Search"> </input>
+
+        </div>)
+}
+
+export default Search
