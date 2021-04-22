a8f85ab79245c19274029198349eaeaca26471e8
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-22 00:05:27 -0400

    made a member/creatorPage in the pages folder, connected it to the MemberPageComponent, and properly connected to the signup and login route

---
+++ client/src/components/MemberPage/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MemberPageComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the Creator Page!</h1>
+            </div>
+        )
+    }
+}
+
---
+++ client/src/pages/CreatorPage/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MemberPageComponent from '../../components/MemberPageComponent/index.js';
+
+export default class CreatorPage extends Component {
+    render() {
+        return (
+            <div>
+                <MemberPageComponent />
+            </div>
+        )
+    }
+}
