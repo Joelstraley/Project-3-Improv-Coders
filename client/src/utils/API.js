12c52e98e63cc1fd8363cb8b5397ad0517b388ed
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-22 22:02:49 -0400

    got local storage for authentication in place in the API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,13 +10,25 @@ export default {
       return axios.get(`${BASEURL}/`);
     },
     postLoginRequest: function(loginRequest) {
-      return axios.post(`${BASEURL}/api/users/loginPassport`, {
+      return new Promise((resolve, reject) => {
+        axios.post(`${BASEURL}/api/users/loginPassport`, {
         //   this become req.body in the server.js post request
         email: loginRequest.email,
         password: loginRequest.password
-      });
-      
-
+      }).then(res =>{
+        // console.log(res.data.token)
+        const token = res.data.token;
+        console.log(token)
+        localStorage.setItem("accessToken", token );
+        // console.log("Getting AccessToken", localStorage.getItem("accessToken"));
+        resolve("Yay")
+      })
+      .catch(err =>{
+          console.log(err)
+          reject("nope")
+      })
+     
+    })
     },
     postShowData: function(showData) {
       return axios.post(`${BASEURL}/api/shows`, {
@@ -60,7 +72,9 @@ export default {
         
       // })
     },
-    getCreatorProfile: function(retrieveTokenSession) {
+    getCreatorProfile: function() {
+      var retrieveTokenSession = localStorage.getItem("accessToken");
+      console.log("Getting AccessToken", retrieveTokenSession);
       return axios.get(`${BASEURL}/user/profile?secret_token=${retrieveTokenSession}`);
     },
 
