08b3f5c2a47b9fbc60445d25f5a0a538054f358f
kofithebass <kofi@kofihayford.com>
2021-04-25 19:12:22 -0400

    pushing all changes to my branch

--- client/src/components/BigAButton/index.js
+++ client/src/components/BigAButton/index.js
@@ -4,7 +4,7 @@ export default function BigAButton() {
     return (
         <div className="flex justify-center m-6">
             <button
-                className="bg-blue-700 m-10 text-white text-2xl font-bold p-10 rounded border-4 border-solid border-gray-400 hover:bg-blue-300 hover:text-black hover:opacity-90 transition duration-500 ">
+                className="bg-blue-700 m-10 text-white text-2xl font-bold p-10 rounded-2xl border-4 border-solid border-gray-400 hover:bg-blue-300 hover:text-black hover:opacity-90 transition duration-500 ">
                 Create a New Event!!
         </button>
         </div>
