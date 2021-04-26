9ac0901592435f3157b67be19fd1d6cd5f5dce57
kofithebass <kofi@kofihayford.com>
2021-04-25 20:53:52 -0400

    made some changes to the AboutUs

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import API from "../../utils/API.js";
+const NavbarImage = './assets/ComedyStream.png';
+
+
 export default function Navbar() {
-    const NavbarImage = './assets/ComedyStream.jpg';
+
     // const [redirect, setRedirect] = useState({destination:null});
 
     // const logoutBtn = () => {
@@ -32,17 +35,21 @@ export default function Navbar() {
             {/* <button onClick= {logoutBtn}>Logout</button> */}
             {/* <Link to= "/logout">Logout</Link> */}
 
-            <div>
+            <div className="bg-gradient-to-r from-blue-100 to-blue-300">
+                <div className="flex justify-center bg-gradient-to-r from-blue-100 to-blue-300"><img src={NavbarImage}
+                    alt="this is the Logo" />
+                </div>
                 <div className="flex justify-start p-6 mt-10 border-solid border-4 items-center border-dark-blue bg-blue-200 hover:bg-blue-100 max-w-full h-50">
                     <button className="py-3 px-3 bg-blue-800 hover:bg-blue-500 text-white rounded mx-10 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"> <Link to="/"> Home</Link></button>
 
-                    <div><img src={NavbarImage} alt="this is the Logo" /></div>
+
                     <div className="md:flex items-center justify-end md:flex-1 lg:w-0">
-                        <button className="py-3 px-3 bg-blue-800 hover:bg-blue-500 text-white rounded mx-10 focus:ring-4 focus:ring-blue-900 focus:ring-opacity-50"><Link to="_____">AboutUs </Link></button>
-                        <button className="py-3 px-3 bg-blue-800 hover:bg-blue-500 text-white rounded mx-10 focus:ring-4 focus:ring-blue-900 focus:ring-opacity-50"><Link to="/accountLogin">Login</Link></button>
+                        <button className="py-3 px-3 bg-blue-800 hover:bg-blue-500 text-white text-lg rounded mx-10  focus:ring-blue-900 focus:ring-opacity-50"><Link to="_____">AboutUs </Link></button>
+                        <button className="py-3 px-3 bg-blue-800 hover:bg-blue-500 text-white text-lg rounded mx-10  focus:ring-blue-900 focus:ring-opacity-50"><Link to="/accountLogin">Login</Link></button>
                         {/* <Link to="/contactMe">Contact Me!</Link> */}
 
-                        <button className="py-3 px-3 bg-blue-800 hover:bg-blue-500 text-white rounded mx-10 focus:ring-4 focus:ring-blue-900 focus:ring-opacity-50"><Link to="/accountRegister">Register Account</Link></button>
+                        <button className="py-3 px-3 bg-blue-800 hover:bg-blue-500 text-white rounded mx-10 focus:ring-blue-900 focus:ring-opacity-50">
+                            <Link to="/accountRegister">Register Account</Link></button>
                     </div>
 
                 </div>
