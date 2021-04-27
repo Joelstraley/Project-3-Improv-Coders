d06bdab1d49130389589e9b196855491525f86fd
Kofithebass <73202978+kofihayford@users.noreply.github.com>
2021-04-27 02:19:48 -0400

    fixed carousel reference

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from "../../components/Navbar";
-import Carousel from "../../components/Carousel";
+import ComedyCarousel from "../../components/Carousel";
 import CardContainer from '../../components/CardContainer'
 import CardsToday from '../../components/CardsToday';
 import CardsFuture from "../../components/CardsFuture";
