8bcaa404278494f21be9fefa1a730c65b7fa56f6
Kofithebass <73202978+kofihayford@users.noreply.github.com>
2021-04-27 02:24:58 -0400

    fixed hover background and added Kofi picutres

--- client/src/components/About-Us/index.js
+++ client/src/components/About-Us/index.js
@@ -12,7 +12,7 @@ export default function AboutUs() {
             <h1 className="flex justify-center text-2xl font-bold md:text-4xl"> Meet the Improv Masters Team </h1>
 
             <div className="flex justify-between py-10 mx-15">
-                <div className="imageGillian rounded-2xl border-4 border-solid border-gray-500 bg-blue-100 hover:bg-blue-100 p-5 hover:shadow-inner shadow-2xl"><img
+                <div className="imageGillian rounded-2xl border-4 border-solid border-gray-500 bg-blue-100 hover:bg-indigo-100 p-5 hover:shadow-inner shadow-2xl"><img
                     src={Gillian} className="h-72 rounded-2xl mx-auto" alt="this is Gillian's image" />
                     <p className="flex justify-center my-4 font-bold font-serif text-lg "> Gillian Fitzgerald </p>
                     <div className="flex justify-center">
@@ -22,7 +22,8 @@ export default function AboutUs() {
                     </div>
                 </div>
 
-                <div className="imageKofi rounded-2xl border-4 border-solid border-gray-500 bg-blue-100 hover:bg-blue-100 p-5 hover:shadow-inner shadow-2xl">
+                <div className="imageKofi rounded-2xl border-4 border-solid border-gray-500 bg-blue-100 hover:bg-indigo-100 p-5 hover:shadow-inner shadow-2xl"><img
+                    src={Kofi} className="h-72 rounded-2xl mx-auto" alt="this is Kofi's image" />
                     <p className="flex justify-center my-4 font-bold font-serif text-lg"> Kofi Hayford </p>
                     <div className="flex justify-center">
                         <img className="w-10 m-5 cursor-pointer hover:opacity-75" src={gitlogo} />
@@ -31,7 +32,7 @@ export default function AboutUs() {
                     </div>
                 </div>
 
-                <div className="imageJoel rounded-2xl border-4 border-solid border-gray-500 bg-blue-100 hover:bg-blue-100 p-5 hover:shadow-inner shadow-2xl"><img src={Joel} className="h-72 rounded-2xl mx-auto" alt="this is Joel's image" />
+                <div className="imageJoel rounded-2xl border-4 border-solid border-gray-500 bg-blue-100 hover:bg-indigo-100 p-5 hover:shadow-inner shadow-2xl"><img src={Joel} className="h-72 rounded-2xl mx-auto" alt="this is Joel's image" />
                     <p className="flex justify-center my-4 font-bold font-serif text-lg"> Joel Straley </p>
                     <div className="flex justify-center">
                         <img className="w-10 m-5 cursor-pointer hover:opacity-75" src={gitlogo} />
