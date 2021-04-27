24ca6d4fa70bd76d1acdab3cad18288636c1f16d
kofithebass <kofi@kofihayford.com>
2021-04-27 17:24:47 -0400

    fixed backgrounds

--- client/src/components/CardsAll/index.js
+++ client/src/components/CardsAll/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link, Redirect} from "react-router-dom";
+import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 import API from "../../utils/API";
 import CardFlyer from '../CardFlyer'
 import dayjs from 'dayjs';
@@ -49,27 +49,27 @@ export default class CardAll extends Component {
                   </div>
                   hs full = full card  
           */
-       <div className="p-8">    
-      <div className="grid grid-cols-5 gap-4" id="app" >
-        <h3 className="inline col-span-5 text-black-500 font-bold">All Shows</h3>
-        {this.state.shows.map(show => {
-          return (
-            <div className="inline rounded-lg shadow-lg content-center text-sm md:text-sm lg:text" id="fullCard" key={show._id} >
-              <img src={show.eventImage} alt="" className="rounded-t-lg w-small item" id="cardImage" ></img>
-              <div className="p-6">
-                <h2 className="font-bold mb-2 text-2xl text-white item flex flex-wrap content-center">{show.eventName}</h2>
-                <p className="text-white mb-2 item truncate ...">{show.description}</p>
-                <p className="text-white mb-2 item truncate ...">${show.cost}</p>
-                <p className="text-white mb-2 item truncate ...">{dayjs(show.startTime).format('dddd, M/D')}</p>
-                <Link className="text-white hover:text-black-500 underline text-sm item truncate ..." 
-                to={"/shows/" + show._id}>
-                     See More 
+      <div className="bg-gradient-to-r from-blue-100 to-blue-300 p-8">
+        <div className="grid grid-cols-5 gap-4" id="app" >
+          <h3 className="inline col-span-5 text-black-500 font-bold">All Shows</h3>
+          {this.state.shows.map(show => {
+            return (
+              <div className="inline rounded-lg shadow-lg content-center text-sm md:text-sm lg:text" id="fullCard" key={show._id} >
+                <img src={show.eventImage} alt="" className="rounded-t-lg w-small item" id="cardImage" ></img>
+                <div className="p-6">
+                  <h2 className="font-bold mb-2 text-2xl text-white item flex flex-wrap content-center">{show.eventName}</h2>
+                  <p className="text-white mb-2 item truncate ...">{show.description}</p>
+                  <p className="text-white mb-2 item truncate ...">${show.cost}</p>
+                  <p className="text-white mb-2 item truncate ...">{dayjs(show.startTime).format('dddd, M/D')}</p>
+                  <Link className="text-white hover:text-black-500 underline text-sm item truncate ..."
+                    to={"/shows/" + show._id}>
+                    See More
                     </Link>
-                    </div>
-            </div>)
-        })}
+                </div>
+              </div>)
+          })}
+        </div>
       </div>
-      </div> 
     )
   };
 };
