ca164a00f0fb1255baa59014c9ebd4eb2ba5eb96
Joel Straley <joelstraley@gmail.com>
2022-12-09 17:36:16 -0500

    updating baseURL

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,45 +1,47 @@
-import axios from "axios";
+import axios from 'axios'
 /* require("dotenv").config(); */
 // Local run
-const BASEURL = 'https://comedy-stream.herokuapp.com'
+const BASEURL = 'https://endearing-llama-bd87c7.netlify.app'
 // const BASEURL = ""
 
 /* process.env.REACT_APP_API || ; */
 
 export default {
-
-    getHome: function() {
-      return axios.get(`${BASEURL}/`);
-    },
-    postLoginRequest: function(loginRequest) {
-      return new Promise((resolve, reject) => {
-        axios.post(`${BASEURL}/api/users/loginPassport`, {
+  getHome: function () {
+    return axios.get(`${BASEURL}/`)
+  },
+  postLoginRequest: function (loginRequest) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(`${BASEURL}/api/users/loginPassport`, {
           //   this become req.body in the server.js post request
           email: loginRequest.email,
-          password: loginRequest.password
-        }).then(resServer =>{
+          password: loginRequest.password,
+        })
+        .then((resServer) => {
           console.log(resServer)
-          const token = resServer.data.token;
+          const token = resServer.data.token
           console.log(token)
-          localStorage.setItem("accessToken", token );
-          localStorage.setItem("userEmail", resServer.data.email );
-          localStorage.setItem("userLogin", true );
-          
+          localStorage.setItem('accessToken', token)
+          localStorage.setItem('userEmail', resServer.data.email)
+          localStorage.setItem('userLogin', true)
+
           // console.log("Getting AccessToken", localStorage.getItem("accessToken"));
           resolve(resServer)
         })
-        .catch(err =>{
-            console.log(err)
-            reject("nope")
-      })
-     
+        .catch((err) => {
+          console.log(err)
+          reject('nope')
+        })
     })
-    },
+  },
 
-    postShowData: function(showData) {
-      var retrieveTokenSession = localStorage.getItem("accessToken");
-      console.log("Getting AccessToken", retrieveTokenSession);
-      return axios.post(`${BASEURL}/secure/createshow?secret_token=${retrieveTokenSession}`, {
+  postShowData: function (showData) {
+    var retrieveTokenSession = localStorage.getItem('accessToken')
+    console.log('Getting AccessToken', retrieveTokenSession)
+    return axios.post(
+      `${BASEURL}/secure/createshow?secret_token=${retrieveTokenSession}`,
+      {
         //   this become req.body in the server.js post request
         eventName: showData.eventName,
         eventType: showData.eventType,
@@ -51,59 +53,55 @@ export default {
         eventLink: showData.eventLink,
         eventImage: showData.eventImage,
         cost: showData.cost,
-        eventPlace: showData.eventPlace
-      });
-    },
+        eventPlace: showData.eventPlace,
+      }
+    )
+  },
 
-    
+  getAllShowData: function () {
+    return axios.get(`${BASEURL}/api/shows`)
+  },
 
-    getAllShowData: function() {
-      return axios.get(`${BASEURL}/api/shows`)
-    },
+  getTodaysShowData: function (dateQuery) {
+    return axios.post(`${BASEURL}/api/shows/today`, {
+      date: dateQuery,
+    })
+  },
 
-    getTodaysShowData: function(dateQuery) {
-      return axios.post(`${BASEURL}/api/shows/today`, {
-              date: dateQuery
-      });
-    },
+  getSingleShowData: function (id) {
+    return axios.get(`${BASEURL}/api/shows/` + id)
+  },
 
-    getSingleShowData: function(id) {
-      return axios.get(`${BASEURL}/api/shows/` +  id);
-    },
-
-    /* search: function(query) {
+  /* search: function(query) {
       return axios.get(`${BASEURL}/api/shows?q` + query);
       },  */
 
+  postSignUpRequest: function (signUpRequest) {
+    return axios.post(`${BASEURL}/api/users/signupPassport`, {
+      //   this become req.body in the server.js post request
+      email: signUpRequest.email,
+      password: signUpRequest.password,
+    })
+  },
+  getCreatorProfile: function () {
+    var retrieveTokenSession = localStorage.getItem('accessToken')
+    console.log('Getting AccessToken', retrieveTokenSession)
+    return axios.get(
+      `${BASEURL}/secure/profile?secret_token=${retrieveTokenSession}`
+    )
+  },
 
-    postSignUpRequest: function(signUpRequest) {
-      return axios.post(`${BASEURL}/api/users/signupPassport`, {
-        //   this become req.body in the server.js post request
-        email: signUpRequest.email,
-        password: signUpRequest.password
-      });
-    
-    },
-    getCreatorProfile: function() {
-      var retrieveTokenSession = localStorage.getItem("accessToken");
-      console.log("Getting AccessToken", retrieveTokenSession);
-      return axios.get(`${BASEURL}/secure/profile?secret_token=${retrieveTokenSession}`);
-    },
-
-    logoutSession: function(){
-      return new Promise((resolve, rej) => {
-        localStorage.removeItem("accessToken");
-        resolve ("logged out")
-      })
-      
-    },
-    getAllCreatorShows: function() {
-      const creatorEmail= localStorage.getItem("userEmail");
-      return axios.get(`${BASEURL}/api/users/allshows/${creatorEmail}`);
-      // var retrieveTokenSession = localStorage.getItem("accessToken");
-      // console.log("Getting AccessToken", retrieveTokenSession);
-      // return axios.get(`${BASEURL}/secure/createshow?secret_token=${retrieveTokenSession}`, {}
-    },
-  
-};
-  
+  logoutSession: function () {
+    return new Promise((resolve, rej) => {
+      localStorage.removeItem('accessToken')
+      resolve('logged out')
+    })
+  },
+  getAllCreatorShows: function () {
+    const creatorEmail = localStorage.getItem('userEmail')
+    return axios.get(`${BASEURL}/api/users/allshows/${creatorEmail}`)
+    // var retrieveTokenSession = localStorage.getItem("accessToken");
+    // console.log("Getting AccessToken", retrieveTokenSession);
+    // return axios.get(`${BASEURL}/secure/createshow?secret_token=${retrieveTokenSession}`, {}
+  },
+}
